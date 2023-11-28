0c8984100a0e981d0e7b9299d7daf4bd15471a69
simone.puce <simone.puce@finconsgroup.com>
2023-11-28 15:43:47 +0100

    First Commmit

---
+++ src/main/java/com/fincons/FinconsEmployeesBeApplication.java
@@ -0,0 +1,13 @@
+package com.fincons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinconsEmployeesBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinconsEmployeesBeApplication.class, args);
+	}
+
+}
