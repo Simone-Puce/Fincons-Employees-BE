e4e73b7cc23d9da6e59254bc2bf9c7fabdced3f7
simone.puce <simone.puce@finconsgroup.com>
2023-12-21 15:22:46 +0100

    Should work on reverse proxy

--- src/main/java/com/fincons/controller/UserController.java
+++ src/main/java/com/fincons/controller/UserController.java
@@ -5,6 +5,7 @@ import com.fincons.exception.DuplicateEmailException;
 import com.fincons.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
