6bd8b906d47c938f6ea7df6fd102a66cec868b5d
michelangelo.masiell <michelangelo.masiello@finconsgroup.com>
2024-01-11 12:54:25 +0100

    Changed AP, implemented test for upload-file method with mockito

--- src/test/java/com/fincons/services/FileServiceImplTest.java
+++ src/test/java/com/fincons/services/FileServiceImplTest.java
@@ -1,29 +1,83 @@
 package com.fincons.services;
 
 
-import org.junit.jupiter.api.Test;
+import com.fincons.entities.Employee;
+import com.fincons.entities.File;
+import com.fincons.mappers.FileMapper;
+import com.fincons.models.FileDTO;
+import com.fincons.repositories.FileRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+public class FileServiceImplTest {
 
+    @InjectMocks
+    private FileServiceImpl fileService;
 
-class FileServiceImplTest {
+    @Mock
+    private FileRepository repository;
 
+    @Mock
+    private FileMapper mapper;
 
-    @Test
-    void uploadFile() {
-
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void viewFile() {
+    public void should_succesfully_upload_a_file() {
 
-    }
+        //given
+        FileDTO dto = new FileDTO(
+                "file64",
+                "name",
+                "extension",
+                "description",
+                new Employee()
+        );
+        File file = new File(
+                "file64",
+                "name",
+                "extension",
+                "description",
+                new Employee()
+        );
+        File fileUploaded = new File(
+                "file64",
+                "name",
+                "extension",
+                "description",
+                new Employee()
+        );
+        fileUploaded.setId(1L);
+//mock the calls
+        Mockito.when(mapper.mapFileDtotoFile(dto)).thenReturn(file);
+        Mockito.when(repository.save(file)).thenReturn(fileUploaded);
+        when(mapper.mapFileToFileDto(fileUploaded)).thenReturn(new FileDTO("file64",
+                "name",
+                "extension",
+                "description",
+                new Employee())
+        );
 
-    @Test
-    void downloadFile() {
-    }
 
-    @Test
-    void getAllFiles() {
+        //when
+        File uploadedFile = fileService.uploadFile(dto);
+
+
+        //then
+        assertEquals(dto.getFile64(), uploadedFile.getFile64());
+        assertEquals(dto.getName(), uploadedFile.getName());
+        assertEquals(dto.getExtension(), uploadedFile.getExtension());
+        assertEquals(dto.getDescription(), uploadedFile.getDescription());
+        //assertEquals(dto.getEmployeeId(), uploadedFile.getEmployee());
     }
 }
