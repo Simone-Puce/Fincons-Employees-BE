9ffa116f99671df11385c15acbf10908319d29de
NarcisVerdesca01 <narcis.verdesca@finconsgroup.com>
2024-01-11 15:43:41 +0100

    Added endpoint to filterChain

--- src/main/java/com/fincons/security/SecurityConfiguration.java
+++ src/main/java/com/fincons/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -40,43 +41,36 @@ public class SecurityConfiguration {
         http
                 .authorizeHttpRequests((authz) -> authz
                         //Auth for App
-                        .requestMatchers(HttpMethod.GET,"/company-employee-management/v1/department/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.POST,"/company-employee-management/v1/department/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.PUT,"/company-employee-management/v1/department/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.DELETE,"/company-employee-management/v1/department/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.DELETE,"/company-employee-management/v1/department/**").hasRole("USER") // Will replace with admin when FE will be ready
+                        .requestMatchers(HttpMethod.DELETE,"/company-employee-management/v1/employee/**").hasRole("USER") // Will replace with admin when FE will be ready
+                        .requestMatchers(HttpMethod.DELETE,"/company-employee-management/v1/position/**").hasRole("USER") // Will replace with admin when FE will be ready
+                        .requestMatchers(HttpMethod.DELETE,"/company-employee-management/v1/project/**").hasRole("USER")  // Will replace with admin when FE will be ready
 
-                        .requestMatchers(HttpMethod.GET,"/company-employee-management/v1/employee/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.POST,"/company-employee-management/v1/employee/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.PUT,"/company-employee-management/v1/employee/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.DELETE,"/company-employee-management/v1/employee/**").hasRole("USER")
+                        .requestMatchers("/company-employee-management/v1/department/**").hasRole("USER")
+                        .requestMatchers("/company-employee-management/v1/employee/**").hasRole("USER")
+                        .requestMatchers("/company-employee-management/v1/position/**").hasRole("USER")
+                        .requestMatchers("/company-employee-management/v1/project/**").hasRole("USER")
 
-                        .requestMatchers(HttpMethod.GET,"/company-employee-management/v1/position/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.POST,"/company-employee-management/v1/position/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.PUT,"/company-employee-management/v1/position/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.DELETE,"/company-employee-management/v1/position/**").hasRole("USER")
-
-                        .requestMatchers(HttpMethod.GET,"/company-employee-management/v1/project/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.POST,"/company-employee-management/v1/project/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.PUT,"/company-employee-management/v1/project/**").hasRole("USER")
-                        .requestMatchers(HttpMethod.DELETE,"/company-employee-management/v1/project/**").hasRole("USER")
 
                         //Auth for Login/Reg
-                        .requestMatchers("/company-employee-management/v1/email").permitAll()//
+                        .requestMatchers("/company-employee-management/v1/email").permitAll()
                         .requestMatchers("/company-employee-management/v1/session-value").permitAll()
                         .requestMatchers("/company-employee-management/v1/home").permitAll()
                         .requestMatchers("/company-employee-management/v1/register").permitAll()
-                        .requestMatchers("/company-employee-management/v1/employees").authenticated() //working
-                        .requestMatchers("/company-employee-management/v1/login").permitAll()
-                        .requestMatchers("/company-employee-management/v1/logout").permitAll()
+                        .requestMatchers("/company-employee-management/v1/employees").authenticated()
                         .requestMatchers("/company-employee-management/v1/error").permitAll()
                         .requestMatchers("/company-employee-management/v1/registered-users").hasAnyRole("ADMIN","USER")
-                        .requestMatchers("/company-employee-management/v1/admin/**").hasRole("ADMIN")
+
+                        .requestMatchers("/company-employee-management/v1/login").permitAll()
+                        .requestMatchers("/company-employee-management/v1/logout").permitAll().anyRequest().authenticated()
+
+
                 );
         http
                 .formLogin(form -> form
                         .loginPage("/company-employee-management/v1/login")
                         .loginProcessingUrl("/company-employee-management/v1/login")
-                        .failureUrl("/company-employee-management/v1/error") //pagine di errore
+                        .failureUrl("/company-employee-management/v1/error")
                         .defaultSuccessUrl("/company-employee-management/v1/home").permitAll());
         http
                 .logout(logout -> logout
