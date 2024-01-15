9e58a05aa2815f8700411bac7b6da1ee0e391f3a
NarcisVerdesca01 <narcis.verdesca@finconsgroup.com>
2024-01-15 09:36:38 +0100

    code review

--- src/main/java/com/fincons/dto/UserDTO.java
+++ src/main/java/com/fincons/dto/UserDTO.java
@@ -2,10 +2,12 @@ package com.fincons.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Getter
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,48 +25,24 @@ public class UserDTO {
     private List<RoleDTO> roles;
 
 
-    public long getId() {
-        return id;
-    }
-
     public void setId(long id) {
         this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public List<RoleDTO> getRoles() {
-        return roles;
     }
 
     public void setRoles(List<RoleDTO> roles) {
