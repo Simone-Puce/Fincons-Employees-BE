16997d36b27d04b628b598881a64a81b6c7f5c09
NarcisVerdesca01 <narcis.verdesca@finconsgroup.com>
2024-01-26 16:01:20 +0100

    Created CustomResponseEntity

---
+++ src/main/java/com/fincons/utility/CustomResponseEntity.java
@@ -0,0 +1,22 @@
+package com.fincons.utility;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomResponseEntity<T> {
+
+    private HttpStatus status;
+    private boolean success;
+    private String message;
+    private T data;
+
+}
