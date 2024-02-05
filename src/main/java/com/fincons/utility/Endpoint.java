95247bc61a706487617512b0906cd5cdd6983773
NarcisVerdesca01 <narcis.verdesca@finconsgroup.com>
2024-02-05 17:14:54 +0100

    Added @Value in SecurityConfiguration for filterChain's endpoints

--- src/main/java/com/fincons/utility/Endpoint.java
+++ src/main/java/com/fincons/utility/Endpoint.java
@@ -1,8 +1,12 @@
 package com.fincons.utility;
 
+import com.fincons.entity.Role;
+import com.fincons.enums.RoleEndpoint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -10,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class Endpoint {
 
     public String path;
-    public String roles;
+
+    public List<RoleEndpoint> roles;
 
 }
