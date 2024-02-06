814589c8427b045bcf08b0cf35bdb3e5bc995ff2
LorenzoGaetaniFinc <lorenzo.gaetani@finconsgroup.com>
2024-02-06 16:41:05 +0100

    commented department test

--- src/test/java/com/fincons/serviceTest/DepartmentServiceTest.java
+++ src/test/java/com/fincons/serviceTest/DepartmentServiceTest.java
@@ -49,10 +49,10 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
 @SpringBootTest
-@ContextConfiguration(classes = DepartmentServiceTest.TestConfig.class)
+//@ContextConfiguration(classes = DepartmentServiceTest.TestConfig.class)
 public class DepartmentServiceTest {
 
-
+    /*
     static class TestConfig {
     }
 
@@ -217,4 +217,6 @@ public class DepartmentServiceTest {
         }
     }
      */
+
 }
+
